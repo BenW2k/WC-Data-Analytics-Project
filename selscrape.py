6e6fc273e8ad6603483d1b7bedfee3f5cc2045c7
BenW2k <benworkman16@gmail.com>
2022-12-29 13:30:36 +0000

    added selenium scraper, discontinuing other scrape

--- selscrape.py
+++ selscrape.py
@@ -4,10 +4,21 @@ from selenium.webdriver.common.by import By
 url = 'https://www.whoscored.com/Regions/247/Tournaments/36/Seasons/8213/Stages/18657/PlayerStatistics/International-FIFA-World-Cup-2022'
 
 driver = webdriver.Chrome()
-
 driver.get(url)
+element = driver.find_element(By .XPATH, '//*[@id="qc-cmp2-ui"]/div[2]/div/button[2]/span')
+element.click()
 
-table = driver.find_element(By.CLASS_NAME, 'grid with-centered-columns hover')
+def getSummaryStatistics(driver):
+  
+  driver.find_element(By .XPATH, '//*[@id="player-table-statistics-body"]/tr[1]/td[1]/a[1]/span').text.strip())
+# table = driver.find_element(By.CLASS_NAME, 'grid with-centered-columns hover')
+# name = driver.find_element(By.XPATH, '//*[@id="player-table-statistics-body"]/tr[1]/td[1]/a[1]/span/text()')
+# # # //*[@id="player-table-statistics-body"]/tr[1]/td[1]/a[1]/span/text()
+# # # //*[@id="player-table-statistics-body"]/tr[2]/td[1]/a[1]/span/text()
 
-for x in range(1,5):
-    
+
+# def get_data(url):
+#   driver.get(url)
+#   driver.quit()
+
+# get_data()
